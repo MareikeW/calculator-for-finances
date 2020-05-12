c97f26156c8b6f0443a9bc4af939fd5c0f9e8b9d
49586574 <mareikewendler@web.de>
2020-05-12 17:49:52 +0200

    Funktion für Rechner

--- chart.js
+++ chart.js
@@ -1,3 +1,5 @@
+// Graph
+
 // für canvas braucht man immer einen Kontext
 let myChart = document.getElementById('myChart').getContext('2d');
 
@@ -5,21 +7,60 @@ let myChart = document.getElementById('myChart').getContext('2d');
 let lineChart = new Chart(myChart, {
   type: 'line',
   data: {
-    labels: [ '1.Jahr', '2.Jahr', '3.Jahr' ],
+    labels: [ 'Anfangskapital', '1.Jahr', '2.Jahr', '3.Jahr' ],
     datasets: [{
       label: 'Einnahmen',
       data: [
         1000,
-        3000,
-        2000
+        2800,
+        2000,
+        3000
       ],
-      backgroundColor: 'pink'
+      borderColor: 'pink',
+      fill: false
     }]
   },
   options: {
+    responsive: true,
+    scales: {
+      yAxes: [{
+        display: true,
+        ticks:
+        {
+          beginAtZero: true
+        }
+      }]
+    },
     title: {
       display: true,
       text: 'Einnahmen in drei Jahren'
     }
   }
 });
+
+// Nach Klick auf "Berechnen" wird das Endkapital berechnet und ausgegeben
+function calc() {
+  var start = document.getElementById('anfangskapital').value;
+  var zins = document.getElementById('zinssatz').value;
+  var zeit = document.getElementById('laufzeit').value;
+
+
+  //berechneEndkapital(start, zins, laufzeit);
+
+  var ergebnis = start + 1;
+
+  document.getElementById('endkapital').innerHTML = ergebnis;
+
+  return false;
+}
+
+//function  berechneEndkapital({ start, zins, laufzeit }) {
+    //var i = 0;
+
+  //  while (i < laufzeit) {
+  //    var ergebnis += start * (1 + (zins/100));
+  //    i++;
+  //  }
+  //  return ergebnis;
+
+  }
